f60b0305940cf8c098bb0e898a5f4adf012ffae4
Adam Lancaster <lancaster28@hotmail.com>
2022-06-27 10:42:36 +0100

    Try the SEEN approach for the straight to struct. NEEDS testing

--- lib/xml/saxy_struct.ex
+++ lib/xml/saxy_struct.ex
@@ -4,14 +4,6 @@ defmodule DataSchema.XML.SaxyStruct do
   that appear in a data schema. It builds a simple form representation of the XML, but
   only puts in elements and attributes that exist in a schema. Check the tests for examples
   of what the schema should look like (it should be a tree that mirrors the structure).
-
-
-  Is there value in the casted simple form? I think it would mean failing earlier which
-  could be good if you could then point to the exact tag etc that failed. You;d be able to
-  point to the tag but not the exact spot, like Nth Offer or whatever. At least not without
-  more overhead. Whereas the errors when casting can at least point to the specific field.
-
-
   """
 
   @behaviour Saxy.Handler
@@ -27,51 +19,25 @@ defmodule DataSchema.XML.SaxyStruct do
   # we close the skipped element and the count is 0.
   # An alternative would be to add a {:skipped  tuple for every nested duplicate tag but I
   # think this would be a larger memory footprint as a tuple > an int.
-  def handle_event(:start_element, {tag_name, _}, {schemas, [{:skip, count, tag_name} | stack]}) do
-    {:ok, {schemas, [{:skip, count + 1, tag_name} | stack]}}
+  def handle_event(:start_element, {tag_name, _}, {schemas, [{:skip, count, tag_name} | stack], seen}) do
+    {:ok, {schemas, [{:skip, count + 1, tag_name} | stack], seen}}
   end
 
-  def handle_event(:start_element, _element, {_, [{:skip, _, _} | _]} = state) do
+  def handle_event(:start_element, _element, {_, [{:skip, _, _} | _], _seen} = state) do
     {:ok, state}
   end
 
-  def handle_event(:start_element, {tag_name, attributes}, {schemas, stack} = s) do
+  def handle_event(:start_element, {tag_name, attributes}, {schemas, stack, seen}) do
     [current_schema | rest_schemas] = schemas
 
     case Map.pop(unwrap_schema(current_schema), tag_name, :not_found) do
       {:not_found, _} ->
+        case seen do
+          [{^tag_name, current_seen} | _rest_seen] ->
+            {:stop, {:error, "Saw many expected one!"}}
 
-        # RIGHT the tricky bit then is that now the acc is a map. So we need parent key
-        # AND the current so that we can access the right map.. Unless we like use
-        # Map.values..
-        s|> IO.inspect(limit: :infinity, label: "STATE")
-        [head | _] = stack
-        stack |> IO.inspect(limit: :infinity, label: "hd")
-        tag_name |> IO.inspect(limit: :infinity, label: "TAG")
-        # What are values going to be ? is it all siblings
-        Map.values(head) |> IO.inspect(limit: :infinity, label: "values")
-
-        # We have to know different things for different fields I think. for has_one you
-        # need the parent key AND you need to be able to pair the tag name to that...
-        # For field you need to be able to pair the tag name to the key in the acc...
-
-        # We can't use the same trick without storing more state because the output no
-        # longer corresponds to the input. We could keep the schemas that we are done with.
-        # we could add in the "seen" list again... would that do it? I think that's the
-        # only way...
-
-        with [{_, _, children} | _] <- stack,
-             true <- List.keymember?(children, tag_name, 0) do
-          {:stop, {:error, "Saw many #{tag_name}'s expected one!"}}
-        else
-          # If the stack is empty and we are skipping that means we have skipped the root
-          # node, and therefore the whole document. So we can stop parsing immediately as
-          # we know the values we want are not here!
-          [] ->
-            {:stop, {:error, :not_found}}
-
-          false ->
-            {:ok, {schemas, [{:skip, 0, tag_name} | stack]}}
+          _ ->
+            {:ok, {schemas, [{:skip, 0, tag_name} | stack], seen}}
         end
 
       {{acc, {:all, child_schema}}, sibling_schema} ->
@@ -85,7 +51,7 @@ defmodule DataSchema.XML.SaxyStruct do
 
               attributes ->
                 tag = {tag_name, attributes, []}
-                {:ok, {schemas, [tag | stack]}}
+                {:ok, {schemas, [tag | stack], seen}}
             end
 
           [{_parent_tag, _, _} | _] ->
@@ -101,7 +67,7 @@ defmodule DataSchema.XML.SaxyStruct do
                   Map.put(sibling_schema, tag_name, {:all, child_schema}) | rest_schemas
                 ]
 
-                {:ok, {schemas, [tag | stack]}}
+                {:ok, {schemas, [tag | stack], seen}}
             end
         end
 
@@ -112,7 +78,7 @@ defmodule DataSchema.XML.SaxyStruct do
 
           with_attributes ->
             schemas = [child_schema, sibling_schema | rest_schemas]
-            {:ok, {schemas, [{tag_name, parent_key, :has_one, with_attributes, opts} | stack]}}
+            {:ok, {schemas, [{tag_name, parent_key, :has_one, with_attributes, opts} | stack], seen}}
         end
 
       {{acc, child_schema}, sibling_schema} ->
@@ -123,7 +89,7 @@ defmodule DataSchema.XML.SaxyStruct do
           with_attributes ->
             schemas = [child_schema, sibling_schema | rest_schemas]
             [_ | rest_stack] = stack
-            {:ok, {schemas, [with_attributes | rest_stack]}}
+            {:ok, {schemas, [with_attributes | rest_stack], seen}}
         end
 
       {child_schema, sibling_schema} ->
@@ -135,8 +101,18 @@ defmodule DataSchema.XML.SaxyStruct do
             {:stop, error}
 
           with_attributes ->
+                seen =
+          case seen do
+            [] ->
+              [{tag_name, %{}}]
+
+            [{parent_tag, current_seen} | rest_seen] ->
+              [{tag_name, %{}}, {parent_tag, Map.put(current_seen, tag_name, true)} | rest_seen]
+          end
+
+
             schemas = [child_schema, sibling_schema | rest_schemas]
-            {:ok, {schemas, [with_attributes | rest_stack]}}
+            {:ok, {schemas, [with_attributes | rest_stack], seen}}
         end
     end
   end
@@ -145,7 +121,7 @@ defmodule DataSchema.XML.SaxyStruct do
     {:ok, state}
   end
 
-  def handle_event(:characters, chars, {schemas, stack} = state) do
+  def handle_event(:characters, chars, {schemas, stack, seen} = state) do
     [current_schema | _rest_schemas] = schemas
 
     case Map.get(unwrap_schema(current_schema), :text, :not_found) do
@@ -159,10 +135,10 @@ defmodule DataSchema.XML.SaxyStruct do
               [{parent_tag, parent_key, :has_one, child_acc, opts} | rest_stack] ->
                 updated_child = update_accumulator(child_acc, key, value)
                 updated = {parent_tag, parent_key, :has_one, updated_child, opts}
-                {:ok, {schemas, [updated | rest_stack]}}
+                {:ok, {schemas, [updated | rest_stack], seen}}
 
               [acc | rest_stack] ->
-                {:ok, {schemas, [update_accumulator(acc, key, value) | rest_stack]}}
+                {:ok, {schemas, [update_accumulator(acc, key, value) | rest_stack], seen}}
             end
 
           {:error, _} = error ->
@@ -171,43 +147,56 @@ defmodule DataSchema.XML.SaxyStruct do
     end
   end
 
-  def handle_event(:cdata, chars, {schemas, stack}) do
+  def handle_event(:cdata, chars, {schemas, stack, seen}) do
     [{tag_name, attributes, content} | stack] = stack
     # We probably want to like parse the cdata... But leave like this for now.
     # We also want to only add it if it's in the schema, but until we have a c-data example
     # let's just always include it and see how we need to handle it later?
     current = {tag_name, attributes, [{:cdata, chars} | content]}
-    {:ok, {schemas, [current | stack]}}
+    {:ok, {schemas, [current | stack], seen}}
   end
 
-  def handle_event(:end_element, element_name, {schemas, [{:skip, 0, element_name} | stack]}) do
-    {:ok, {schemas, stack}}
+  def handle_event(
+        :end_element,
+        element_name,
+        {schemas, [{:skip, 0, element_name} | stack], seen}
+      ) do
+    # We need to remove ONLY when we are closing the parent of this tag.
+    # how do we track parent. We have to put it into the stack somewhere.
+
+    case seen do
+      [] ->
+        {:ok, {schemas, stack, seen}}
+
+      [_ | rest_seen] ->
+        {:ok, {schemas, stack, rest_seen}}
+    end
   end
 
-  def handle_event(:end_element, element_name, {schemas, [{:skip, count, element_name} | stack]}) do
-    {:ok, {schemas, [{:skip, count - 1, element_name} | stack]}}
+  def handle_event(:end_element, element_name, {schemas, [{:skip, count, element_name} | stack], seen}) do
+    {:ok, {schemas, [{:skip, count - 1, element_name} | stack], seen}}
   end
 
-  def handle_event(:end_element, _element_name, {_schemas, [{:skip, _, _} | _]} = state) do
+  def handle_event(:end_element, _element_name, {_schemas, [{:skip, _, _} | _], _seen} = state) do
     {:ok, state}
   end
 
   def handle_event(
         :end_element,
         tag_name,
-        {schemas, [{tag_name, parent_key, :has_one, child_acc, _opts}, acc | rest_stack]}
+        {schemas, [{tag_name, parent_key, :has_one, child_acc, _opts}, acc | rest_stack], seen}
       ) do
     [_current_schema | rest_schemas] = schemas
     with_child = update_accumulator(acc, parent_key, child_acc)
-    {:ok, {rest_schemas, [with_child | rest_stack]}}
+    {:ok, {rest_schemas, [with_child | rest_stack], seen}}
   end
 
-  def handle_event(:end_element, _tag_name, {schemas, stack}) do
+  def handle_event(:end_element, _tag_name, {schemas, stack, seen}) do
     [_current_schema | rest_schemas] = schemas
 
     case stack do
       [parent | rest] ->
-        {:ok, {rest_schemas, [parent | rest]}}
+        {:ok, {rest_schemas, [parent | rest], seen}}
     end
   end
 
@@ -219,14 +208,14 @@ defmodule DataSchema.XML.SaxyStruct do
   defp unwrap_schema(%{} = schema), do: schema
 
   def parse_string(data, schema) do
-    state = {[schema], [%{}]}
+    state = {[schema], [%{}], []}
 
     case Saxy.parse_string(data, __MODULE__, state, []) do
       {:ok, {:error, _reason} = error} -> error
       # If we are returned an empty stack that means nothing in the XML was in the schema.
       # If we found even one thing we would be returned a simple form node.
-      {:ok, {_, []}} -> {:error, :not_found}
-      {:ok, {_, [struct]}} -> {:ok, struct}
+      {:ok, {_, [], _}} -> {:error, :not_found}
+      {:ok, {_, [struct], _}} -> {:ok, struct}
     end
   end
 
